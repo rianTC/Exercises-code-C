70f9b8d4a86944da8dad42ede7fb831ba1af711a
rianTC <qkhoi1005@gmail.com>
2023-08-06 21:26:04 +0700

    test 2

--- test 2.cpp
+++ test 2.cpp
@@ -3,7 +3,7 @@
 // Total from -5 to 1
 int main (){
 	int Sum = 0;
-	for(int i = -5; i <= 1; i++){
+	for(int i = 1; i >= -5; i--){
 		Sum += i;
 	}
 	printf("The total from to -5 to 1 is: %d", Sum);
