e20fd8772393d7203a3f2c1aded9c35db3f30abd
rianTC <qkhoi1005@gmail.com>
2023-08-06 13:47:40 +0700

    test 2

--- test 2.cpp
+++ test 2.cpp
@@ -2,12 +2,9 @@
 
 // Total from -5 to 1
 int main (){
-	int Total;
-	printf("Enter a natural number: ");
-	scanf("%d", &Total);
 	int Sum = 0;
-	for(int i = -5; i <= Total; i++){
+	for(int i = -5; i <= 1; i++){
 		Sum += i;
 	}
-	printf("Total is: %d", Sum);
+	printf("The total from to -5 to 1 is: %d", Sum);
 }
