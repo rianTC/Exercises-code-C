8cf96e703592510ee5bb5f962a3ad42472e7ef11
rianTC <qkhoi1005@gmail.com>
2023-08-06 11:09:03 +0700

    add hello

--- part 1.cpp
+++ part 1.cpp
@@ -3,31 +3,31 @@
 
 // tong cac chu so
 int main () {
-	int n;
+	int tong;
 	printf("nhap so tu nhien : ");
-	scanf("%d", &n);
+	scanf("%d", &tong);
 	int sum = 0;
-	for(int i = 0; i <= n; i++){
+	for(int i = 0; i <= tong; i++){
 		sum += i;	
 	}
-	printf("dap an la: %d\n", sum);
+	printf("tong cac so tu nhien la: %d\n", sum);
 	
 // tong cac chu so le 	
-	int q;
+	int tong_le;
 	printf("nhap so tu nhien: ");
-	scanf("%d", &q);
+	scanf("%d", &tong_le);
 	int so_le = 0;
-	for (int i = 1; i <= n; i += 2){
+	for (int i = 1; i <= tong_le; i += 2){
 			so_le += i;
 	}
 	printf("tong so le la : %d \n", so_le);
 	
 //	tong cac chu so chan
-	int h;
-	printf("nhap so tu nhien :");
-	scanf("%d", &h);
+	int tong_chan;
+	printf("nhap so tu nhien : ");
+	scanf("%d", &tong_chan);
 	int so_chan = 0;
-	for (int i = 0; i <= n; i += 2){
+	for (int i = 0; i <= tong_chan; i += 2){
 		so_chan += i;
 	}
 	printf("tong cac so chan la : %d \n", so_chan);
